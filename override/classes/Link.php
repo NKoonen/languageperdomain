21d8bd3579aae9a062ac74a4f3c827d6740b844f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-11-11 12:43:22 +0100

    Use parent link class and simply replace domain

--- override/classes/Link.php
+++ override/classes/Link.php
@@ -44,38 +44,12 @@ class Link extends LinkCore
      */
     public function getBaseLink($idShop = null, $ssl = null, $relativeProtocol = false)
     {
-        if (null === $ssl) {
-            $ssl = (Configuration::get('PS_SSL_ENABLED') && Configuration::get('PS_SSL_ENABLED_EVERYWHERE'));
-        }
-        if (Configuration::get('PS_MULTISHOP_FEATURE_ACTIVE') && $idShop !== null) {
-            $shop = new Shop($idShop);
-        } else {
-            $shop = Context::getContext()->shop;
-        }
-        if ($relativeProtocol) {
-            $base = '//'.($ssl && $this->ssl_enable ? $shop->domain_ssl : $shop->domain);
-        } else {
-            $base = (($ssl && $this->ssl_enable) ? 'https://'.$shop->domain_ssl : 'http://'.$shop->domain);
-        }
+	    $baseLink = parent::getBaseLink( $idShop, $ssl, $relativeProtocol );
 
-        $orginalPsLink = $base.$shop->getBaseURI();
-
-        $new_extension = Db::getInstance()->getRow(
-            '
-            SELECT *
-            FROM `'._DB_PREFIX_.'languageperdomain`
-            WHERE `lang_id` = '.(int)Context::getContext()->language->id.'
-            AND `target_replace` = '.(int)Context::getContext()->shop->id.'
-            '
-        );
-
-        $controller = Dispatcher::getInstance()->getController();
-        if ($new_extension && $controller != 'AdminOrders') {
-            $LanguagePerDomainLink = str_replace(parse_url($orginalPsLink)["host"], $new_extension["new_target"], $orginalPsLink);
-            return $LanguagePerDomainLink;
-        } else {
-            return $orginalPsLink;
-        }
+		if ( $this->languageperdomain ) {
+			return $this->languageperdomain->replaceDomain( $baseLink );
+		}
+		return $baseLink;
     }
 
     /**
@@ -92,47 +66,13 @@ class Link extends LinkCore
 
     public function getImageLink($name, $ids, $type = null)
     {
-        $notDefault = false;
-        $moduleManagerBuilder = ModuleManagerBuilder::getInstance();
-        $moduleManager = $moduleManagerBuilder->build();
-        static $watermarkLogged = null;
-        static $watermarkHash = null;
-        static $psLegacyImages = null;
-        if ($watermarkLogged === null) {
-            $watermarkLogged = Configuration::get('WATERMARK_LOGGED');
-            $watermarkHash = Configuration::get('WATERMARK_HASH');
-            $psLegacyImages = Configuration::get('PS_LEGACY_IMAGES');
-        }
+		$imageLink = parent::getImageLink( $name, $ids, $type );
 
-        // Check if module is installed, enabled, customer is logged in and watermark logged option is on
-        if (!empty($type) && $watermarkLogged &&
-            ($moduleManager->isInstalled('watermark') && $moduleManager->isEnabled('watermark')) &&
-            isset(Context::getContext()->customer->id)
-        ) {
-            $type .= '-' . $watermarkHash;
-        }
+	    if ( $this->languageperdomain ) {
 
-        // legacy mode or default image
-        $theme = ((Shop::isFeatureActive() && file_exists(_PS_PROD_IMG_DIR_ . $ids . ($type ? '-' . $type : '') . '-' . Context::getContext()->shop->theme_name . '.jpg')) ? '-' . Context::getContext()->shop->theme_name : '');
-        if (($psLegacyImages
-                && (file_exists(_PS_PROD_IMG_DIR_ . $ids . ($type ? '-' . $type : '') . $theme . '.jpg')))
-            || ($notDefault = strpos($ids, 'default') !== false)) {
-            if ($this->allow == 1 && !$notDefault) {
-                $uriPath = __PS_BASE_URI__ . $ids . ($type ? '-' . $type : '') . $theme . '/' . $name . '.jpg';
-            } else {
-                $uriPath = _THEME_PROD_DIR_ . $ids . ($type ? '-' . $type : '') . $theme . '.jpg';
-            }
-        } else {
-            // if ids if of the form id_product-id_image, we want to extract the id_image part
-            $splitIds = explode('-', $ids);
-            $idImage = (isset($splitIds[1]) ? $splitIds[1] : $splitIds[0]);
-            $theme = ((Shop::isFeatureActive() && file_exists(_PS_PROD_IMG_DIR_ . Image::getImgFolderStatic($idImage) . $idImage . ($type ? '-' . $type : '') . '-' . (int) Context::getContext()->shop->theme_name . '.jpg')) ? '-' . Context::getContext()->shop->theme_name : '');
-            if ($this->allow == 1) {
-                $uriPath = __PS_BASE_URI__ . $idImage . ($type ? '-' . $type : '') . $theme . '/' . $name . '.jpg';
-            } else {
-                $uriPath = _THEME_PROD_DIR_ . Image::getImgFolderStatic($idImage) . $idImage . ($type ? '-' . $type : '') . $theme . '.jpg';
-            }
-        }
-        return $this->getBaseLink() . $uriPath;
+		    return $this->languageperdomain->replaceDomain( $imageLink );
+	    }
+
+	    return $imageLink;
     }
 }
